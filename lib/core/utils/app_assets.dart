d88dd1b5a5a30213d787fc1d88a016c12960f02f
AAELBDAWY1318 <abdullahawad1318@gmail.com>
2024-09-22 02:47:56 +0300

    add app assets, colors, and strings

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesForget
+  /// assets/images/forget.png
+  static const String assetsImagesForget = "assets/images/forget.png";
+
+  /// Assets for assetsImagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String assetsImagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for assetsImagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String assetsImagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for assetsImagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String assetsImagesOnboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for assetsImagesSuccess
+  /// assets/images/success.png
+  static const String assetsImagesSuccess = "assets/images/success.png";
+
+  /// Assets for assetsImagesVector
+  /// assets/images/vector.png
+  static const String assetsImagesVector = "assets/images/vector.png";
+
+  /// Assets for assetsImagesVector2
+  /// assets/images/Vector2.png
+  static const String assetsImagesVector2 = "assets/images/Vector2.png";
+}
+
