2051ac27f9340a7ea68b75e5eba4df1992a1af65
AAELBDAWY1318 <abdullahawad1318@gmail.com>
2024-12-07 04:00:11 +0200

    handle state in UI

--- lib/features/souvenirs/presentation/view/souvenirs_view.dart
+++ lib/features/souvenirs/presentation/view/souvenirs_view.dart
@@ -1,4 +1,5 @@
-
+import 'package:dalel/core/functions/show_custom_snack_bar.dart';
+import 'package:dalel/core/utils/app_colors.dart';
 import 'package:dalel/core/widgets/custom_button.dart';
 import 'package:dalel/features/home/data/models/souvenir_model.dart';
 import 'package:dalel/features/home/presentation/widgets/custom_app_bar.dart';
@@ -17,27 +18,48 @@ class SouvenirsView extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0),
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            //!appbar section
-            const SliverToBoxAdapter(child: SizedBox(height: 50.0)),
-            const SliverToBoxAdapter(child: CustomAppBar()),
-            const SliverToBoxAdapter(child: SizedBox(height: 32.0)),
-            SliverToBoxAdapter(
-                child: SouvenirItem(image: data.image,title: data.title)),
-            const SliverToBoxAdapter(child: SizedBox(height: 32.0)),
-            SliverToBoxAdapter(child: CustomPriceWiget(price: data.price)),
-            const SliverToBoxAdapter(child: SizedBox(height: 50.0)),
-            SliverToBoxAdapter(child: CustomButton(text: "ADD TO CARD", onPressed: (){
-              context.read<SouvenirCubit>().addToCart(SouvenirModel(
-                id: data.id, 
-                image: data.image, 
-                price: data.price, 
-                title: data.title, 
-              ));
-            })), 
-          ],
+        child: BlocConsumer<SouvenirCubit, SouvenirState>(
+          listener: (context, state) {
+            if (state is AddToCartFailureState) {
+              showSnackBar(context, text: state.message, color: Colors.red);
+            } else if (state is AddToCartSuccessState) {
+              showSnackBar(context,
+                  text: "Add to cart successfully", color: Colors.green);
+            }
+          },
+          builder: (context, state) {
+            return CustomScrollView(
+              physics: const BouncingScrollPhysics(),
+              slivers: [
+                //!appbar section
+                const SliverToBoxAdapter(child: SizedBox(height: 50.0)),
+                const SliverToBoxAdapter(child: CustomAppBar()),
+                const SliverToBoxAdapter(child: SizedBox(height: 32.0)),
+                SliverToBoxAdapter(
+                    child: SouvenirItem(image: data.image, title: data.title)),
+                const SliverToBoxAdapter(child: SizedBox(height: 32.0)),
+                SliverToBoxAdapter(child: CustomPriceWiget(price: data.price)),
+                const SliverToBoxAdapter(child: SizedBox(height: 50.0)),
+                SliverToBoxAdapter(
+                    child: State is AddToCartLoadingState
+                        ? CircularProgressIndicator(
+                            color: AppColors.primaryColor,
+                          )
+                        : CustomButton(
+                            text: "ADD TO CARD",
+                            onPressed: () {
+                              context
+                                  .read<SouvenirCubit>()
+                                  .addToCart(SouvenirModel(
+                                    id: data.id,
+                                    image: data.image,
+                                    price: data.price,
+                                    title: data.title,
+                                  ));
+                            })),
+              ],
+            );
+          },
         ),
       ),
     );
