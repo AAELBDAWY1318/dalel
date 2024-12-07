aca3d5dcd888438a40265c0d337c083dc06f54ce
AAELBDAWY1318 <abdullahawad1318@gmail.com>
2024-12-07 04:19:22 +0200

    add app bar to cart view

--- lib/features/my_cart/presentation/view/my_cart_view.dart
+++ lib/features/my_cart/presentation/view/my_cart_view.dart
@@ -1,3 +1,5 @@
+import 'package:dalel/core/utils/app_colors.dart';
+import 'package:dalel/core/utils/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class MyCartView extends StatelessWidget {
@@ -5,6 +7,24 @@ class MyCartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        title: Text(
+          "My Cart",
+          style: AppTextStyles.pacifico40064.copyWith(fontSize: 20.0),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.delete,
+              color: AppColors.deepGrey,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
